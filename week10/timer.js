f536e75ce5c70f2580fe58d4b1ada5802374fa02
yurnnseo <ysshinaja@naver.com>
2024-12-08 01:41:07 +0900

    Create timer.js

---
+++ week10/timer.js
@@ -0,0 +1,19 @@
+let count=1;
+let timerID=null; 
+
+onmessage = function (e) {
+	if(e.data == "start") {
+		if(timerID != null) return; 
+		timerID = setInterval(myCallback, 1000); 
+	}
+	else if(e.data == "stop") {
+		if(timerID == null) return; 
+		clearInterval(timerID);
+		close(); 
+	}
+}
+
+function myCallback() { 
+	postMessage(count); 
+	count++; 
+}
