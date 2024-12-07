3cadebe73473ce43b85910cb7763208db835a806
yurnnseo <ysshinaja@naver.com>
2024-12-08 01:52:17 +0900

    Create add.js

---
+++ week10/add.js
@@ -0,0 +1,9 @@
+onmessage = function(e) {
+    let sum = 0;
+    let from = parseInt(e.data.from);
+    let to = parseInt(e.data.to);
+    for(let i=from; i<=to; i++) {
+        sum += i;
+    }
+    this.postMessage(sum);
+}
