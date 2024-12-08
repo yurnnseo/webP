3d6d5321b499176c5163db574e32c4cf43b7634b
yurnnseo <ysshinaja@naver.com>
2024-12-08 18:56:30 +0900

    Update app8.js

--- week14/app8.js
+++ week14/app8.js
@@ -1,9 +1,11 @@
+const express = require('express');
+const path = require('path');
 const bodyParset = require('body-parser');
+const static = require('serve-static');
+const app = express();
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParset.json());
-
-const path = require('path');
-const static = require('serve-static');
 app.use('/', static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
@@ -11,7 +13,10 @@ app.use(function(req, res, next){
     const paramPassword = req.body.password || req.query.password;
 
     res.status(200).send(
-        `<h1></h1>
-        <div></div>
+        `<h1>서버에서 응답한 결과</h1>
+        <div><p>Param id : ${paramId}</p></div>
+        <div><p>Param password : ${paramPassword}</p></div>
+        <br><br><a href = "/login1.html">로그인 페이지로 돌아가기</a>
         `);
 });
+
